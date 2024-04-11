87ac423bfe21642bbdc6f41235eec5d80927d107
Usuario <Usuario@CPD03-M16>
2024-04-11 15:55:52 -0300

    Criacao de classes

---
+++ SistemaBiblioteca/Helpers/SQLiteDatabaseHelpers.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblioteca.Helpers
+{
+    public class SQLiteDatabaseHelpers
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<>
+        }
+    }
+}
