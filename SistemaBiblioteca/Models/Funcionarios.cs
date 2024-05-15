8e1235d521acc48e9f99bf9e665a3ddd9d1b10b8
Arthur Cesar de Freitas <Arthurfreitas2006@hotmail.com>
2024-05-15 19:46:10 -0300

    Criando banco

--- SistemaBiblioteca/Models/Funcionarios.cs
+++ SistemaBiblioteca/Models/Funcionarios.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Android.Views;
+using MySql.Data.MySqlClient;
+using SistemaBiblioteca.Resources.Banco;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,30 @@ namespace SistemaBiblioteca.Models
 {
     public class Funcionarios
     {
-        public int ID { get; set; }
+        public int idFuncionario { get; set; }
         public string Nome { get; set; }
         public string Senha { get; set; }
+
+
+        public async void AdicionarFuncionario()
+        {
+            try
+            {
+                Banco.AbrirConexao();
+                Banco.Comando = new MySqlCommand("INSERT IN TO Funcionario (Nome) VALUES (@Nome)", Banco.Conexao);
+                Banco.Comando.Parameters.AddWithValue("@Nome", Nome);
+                Banco.Comando.ExecuteNonQuery();
+                Banco.FecharConexao();
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        public static void DisplayAlert(string v1, string message, string v2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ SistemaBiblioteca/Resources/Banco/Banco.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.OS;
+using Android.Views;
+using MySql.Data.MySqlClient;
+using Mysqlx;
+
+namespace SistemaBiblioteca.Resources.Banco
+{
+    public class Banco
+    {
+        public static MySqlConnection Conexao;
+        public static MySqlCommand Comando;
+        public static MySqlDataAdapter Adaptador;
+        public static DataTable datTable;
+
+        public static void DisplayAlert(string v1, string message, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void AbrirConexao()
+        {
+            try
+            {
+                Conexao = new MySqlConnection("server=localhost;port=3307;uid=root,pwd=etecjau");
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        public static void FecharConexao()
+        {
+            try
+            {
+                Conexao.Close();
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        public static void CriarBanco()
+        {
+            try
+            {
+                AbrirConexao();
+                Comando = new MySqlCommand("create database if not exists Biblioteca;use Biblioteca;", Conexao);
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand("create table if not exists Funcionario(idFuncionario int auto_increment,Nome varchar(40),Senha varchar(40),primary key (idFuncionario));", Conexao);
+                Comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+    }
+}
