29c3264b1c6db86b5dc202efe10b522a95316f0f
Usuario <Usuario@CPD03-M16>
2024-05-15 15:55:44 -0300

    Criando adicionar funcionario

---
+++ SistemaBiblioteca/Views/CadastroFuncionario.xaml.cs
@@ -0,0 +1,14 @@
+namespace SistemaBiblioteca.Views;
+
+public partial class CadastroFuncionario : ContentPage
+{
+	public CadastroFuncionario()
+	{
+		InitializeComponent();
+	}
+
+    private void AdicionarFuncionario(object sender, EventArgs e)
+    {
+
+    }
+}
